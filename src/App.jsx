7d2d7af78b3e71093bea2055c5c1adc9d7a937dd
TanKittipat <kittipat12tan@gmail.com>
2024-03-14 08:05:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import BMICalculatorComponent from "./BMICalculatorComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <BMICalculatorComponent />
+    </div>
+  );
+}
+
+export default App;
